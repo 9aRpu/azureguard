e5f8b9b77fe35cbb3279ef5ccbef3a2976a43b7b
Thijs <thijs@me.com>
2021-10-21 21:44:32 +0200

    added printing of exit code

--- scripts/subinstallers/check_updates.sh
+++ scripts/subinstallers/check_updates.sh
@@ -7,6 +7,5 @@ UPDATES=$(apt-get dist-upgrade -s --quiet=2 | grep -c ^Inst)
 if [[ "$UPDATES" -ne "0" ]]; then
     echo "Please run updates and reboot before installing azureguard: sudo apt-get update && sudo apt-get -y dist-upgrade"
     exit 1;
-else
-    echo "No updates"
 fi
+printf '%d\n' $?
