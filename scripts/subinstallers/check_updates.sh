1cd0d2a3d7c83390a684c9f36c1fd505e718b8c0
Thijs <thijs@me.com>
2021-10-21 22:00:10 +0200

    no clue what changed

--- scripts/subinstallers/check_updates.sh
+++ scripts/subinstallers/check_updates.sh
@@ -2,9 +2,9 @@
 
 sudo apt-get update
 
-UPDATES=$(sudo apt-get dist-upgrade -s --quiet=2 | grep -c ^Inst)
+UPDATES=$(sudo apt-get dist-upgrade -s --quiet=2 | grep ^Inst | wc -l)
 
 if [[ "$UPDATES" -ne "0" ]]; then
-    echo "Please run updates and reboot before installing azureguard: sudo apt-get update && sudo apt-get -y dist-upgrade"
+    echo "Please run updates and reboot before installing Mistborn: sudo apt-get update && sudo apt-get -y dist-upgrade"
     exit 1;
 fi
